aeb3657f1c4a38d97da31c6a54e591919f3e6948
L.R.L <DaleLoreNY@gmail.com>
2019-08-11 18:47:04 -0400

    Finished auth part 1 for backend api

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,3 +1,18 @@
 class AuthController < ApplicationController
 
+  def create
+    user = User.find_by(username: params[:username])
+    is_authenticated = user.authenticate(params[:password])
+
+    if user && is_authenticated
+      payload = { user_id: user.id }
+      token = JWT.encode(payload, 'secret', 'HS256')
+      render json: { token: token }
+    else
+      render json: { error: ["Nice try boi"] }, status: :unprocessable_entity
+    end
+
+  end
+
+
 end
