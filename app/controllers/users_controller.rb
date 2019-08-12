435e2bbeb319a6f284dfb2789327a6e2a868f54f
L.R.L <DaleLoreNY@gmail.com>
2019-08-11 20:23:48 -0400

    Finished auth with refactoring in the backend. Only issue is that I had to change to a switch statement to follow guidelines. But will refactor the frontend with BrowserRouter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,25 +8,14 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.valid?
-      payload = { user_id: user.id }
-      token = JWT.encode(payload, 'secret', 'HS256')
-
-      render json: { token: token }
+      render json: { token: encode_token(user)}
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def profile
-    token = request.headers["Authorization"]
-
-    decoded_token = JWT.decode(token, 'secret', true, { algorithm: 'HS256' })
-
-    user_id = decoded_token[0]["user_id"]
-
-    user = User.find(user_id)
-    
-    render json: user
+    render json: current_logged_in_user
   end
 
 
