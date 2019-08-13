bd0e2e7c5fc35f697ebf213b83c946dbb2718ca6
L.R.L <DaleLoreNY@gmail.com>
2019-08-13 16:03:51 -0400

    Testing to see if I can delete

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -10,4 +10,28 @@ class CollectionsController < ApplicationController
     render json: collection
   end
 
+  def create
+    collection = Collection.create(collection_params)
+    collection.user = current_logged_in_user
+      if collection.save
+        render json: collection
+      else
+        render json: { errors: collection.errors.full_messages }, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    if @collection.user == current_logged_in_user
+      @collection = Collection.find(params[:id])
+      @collection.destroy
+    else
+      render json: { errors: ["You cannot delete"] }, status: :unauthorized
+    end
+  end
+
+  private
+  def collection_params
+    params.permit(:collection_name, :description)
+  end
+
 end
