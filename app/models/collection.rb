e5c1d5aa000ba431d54d637a5edbcad8341a0c78
L.R.L <DaleLoreNY@gmail.com>
2019-08-21 17:34:14 -0400

    Clean up

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,14 @@
 class Collection < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
+
+  def self.to_csv(fields = column_names, options = {})
+    CSV.generate(options) do |csv|
+      csv << fields
+      all.each do |collection|
+        csv << collection.attributes.values_at(*fields)
+      end
+    end
+  end
+
 end
